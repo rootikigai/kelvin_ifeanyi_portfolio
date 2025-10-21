5d86e50a5bcb04976137284fe73173ff495bf584
rootikigai <ifykel360@gmail.com>
2025-10-21 23:47:57 +0100

    Added page turning functionality: implemented click events for next and previous buttons to navigate pages.

---
+++ script.js
@@ -0,0 +1,22 @@
+//I want to be able to turn pages when I click the next or prev (chevron) buttons
+const pageTurnBtn = document.querySelectorAll('.nextprev-btn');
+
+pageTurnBtn.forEach((el, index) => {
+    el.onclick = () => {
+        const pageTurnId = el.getAttribute('data-page');
+        const pageTurn = document.getElementById(pageTurnId);
+
+        if (pageTurn.classList.contains('turn')) {
+            pageTurn.classList.remove('turn');
+            setTimeout(() => {
+                pageTurn.style.zIndex = 20 - index;
+            }, 500)
+        } 
+        else {
+            pageTurn.classList.add('turn');
+            setTimeout(() => {
+                pageTurn.style.zIndex = 20 + index;
+            }, 500)
+        }
+    };
+});
