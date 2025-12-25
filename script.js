6d61f2f46b8f7793309eb28a7819a5871bd39bb7
rootikigai <ifykel360@gmail.com>
2025-12-25 15:55:34 +0100

    Update

--- script.js
+++ script.js
@@ -2,24 +2,24 @@
 const pageTurnBtn = document.querySelectorAll('.nextprev-btn');
 
 pageTurnBtn.forEach((el, index) => {
-    el.onclick = () => {
-        const pageTurnId = el.getAttribute('data-page');
-        const pageTurn = document.getElementById(pageTurnId);
+  el.onclick = () => {
+    const pageTurnId = el.getAttribute('data-page');
+    const pageTurn = document.getElementById(pageTurnId);
 
-        if (pageTurn.classList.contains('turn')) {
-            // Turn page backwards
-            pageTurn.classList.remove('turn');
-            setTimeout(() => {
-                pageTurn.style.zIndex = 20 - index;
-            }, 500);
-        } else {
-            // Turn page forwards
-            pageTurn.classList.add('turn');
-            setTimeout(() => {
-                pageTurn.style.zIndex = 20 + index;
-            }, 500);
-        }
-    };
+    if (pageTurn.classList.contains('turn')) {
+      // Turn page backwards
+      pageTurn.classList.remove('turn');
+      setTimeout(() => {
+        pageTurn.style.zIndex = 20 - index;
+      }, 500);
+    } else {
+      // Turn page forwards
+      pageTurn.classList.add('turn');
+      setTimeout(() => {
+        pageTurn.style.zIndex = 20 + index;
+      }, 500);
+    }
+  };
 });
 
 
